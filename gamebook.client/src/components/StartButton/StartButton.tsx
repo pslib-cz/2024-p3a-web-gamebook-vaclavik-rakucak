68fcd7a3310d859411c2ee488a26408042f17c0f
Jakub <jakub.vaclavik.022@pslib.cz>
2024-12-15 18:43:36 +0100

    another set of changes, but I feel so useless and tired... Be a programmer isn't a job or dream, but lifestyle

--- gamebook.client/src/components/StartButton/StartButton.tsx
+++ gamebook.client/src/components/StartButton/StartButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './StartButton.module.css';
+import styles from '../StartButton/StartButton.module.css';
 
 const StartButton: React.FC = () => {
     const navigate = useNavigate();
--- gamebook.client/src/pages/MenuPage/MenuPage.tsx
+++ gamebook.client/src/pages/MenuPage/MenuPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import StartButton from '../../components/StartButton';
-import styles from './MenuPage.module.css';
+import StartButton from '../../components/StartButton/StartButton';
+import styles from '../MenuPage/MenuPage.module.css';
 
 const MenuPage: React.FC = () => {
     return (
         <div className={styles.menuPage}>
-            <h1 className={styles.title}>Dungeonborne</h1>
+            <h1 className={styles.title}>Dungeon'borne</h1>
             <div className={styles.buttonContainer}>
                 <StartButton />
             </div>
