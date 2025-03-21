a98f933e25d8f6fed545adf60c8e01aa65b41fae
Jiří Rakučák <jiri.rakucak.022@pslib.cz>
2025-03-21 09:45:50 +0100

    nevim

--- gamebook.client/src/api/imagesApi.ts
+++ gamebook.client/src/api/imagesApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL + '/images';
+const BASE_URL ='/api/images';
 
 export const fetchImage = async (imageId: number) => {
   try {
